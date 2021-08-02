df4588e87fd93baae78fc258d16d6e73f181d5cd
Ryel Banfield <ryelbanfield@gmail.com>
2021-08-02 16:03:07 -0400

    Build search form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,28 @@
-import '../API';
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+import { fetchPhotosBySearch, fetchRandomPhoto, fetchLatestPhotos } from '../API';
 
-const App = () => (
-  <div className="App">
-    <h1>Hello World</h1>
-  </div>
-);
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState(null);
+  const [randomPhoto, setRandomPhoto] = useState(null);
+  const [latestPhotos, setLatestPhotos] = useState(null);
+
+  return (
+    <main className="home">
+      <div className="home-search">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="button" className="search-button" onClick={() => fetchPhotosBySearch(searchTerm)}>
+          Search
+        </button>
+      </div>
+      <h1>Hello World</h1>
+    </main>
+  );
+};
 
 export default App;
