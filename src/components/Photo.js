06f2a25051931e82a5cca72ea8d44b2914ebba55
Ryel Banfield <ryelbanfield@gmail.com>
2021-08-05 10:20:16 -0400

    Finish MVP

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -12,8 +12,47 @@ const Photo = () => {
     <main className="h-photo">
       {photo && (
       <div className="photo">
-        <h1>{photo.id}</h1>
+        {photo.description && (
+          <>
+            <p className="photo-description-h">
+              Description:
+            </p>
+            <p className="photo-description">
+              {photo.description}
+            </p>
+          </>
+        )}
+        <p className="photographer">
+          Photographer:
+          {' '}
+          {photo.user.name}
+        </p>
         <img src={photo.urls.small} alt={photo.alt_description} />
+        <p className="photo-info">
+          {photo.likes && (
+            <>
+              Likes:
+              {' '}
+              {photo.likes}
+              {' '}
+            </>
+          )}
+          {photo.downloads && (
+            <>
+              Downloads:
+              {' '}
+              {photo.downloads}
+              {' '}
+            </>
+          )}
+          {photo.views && (
+            <>
+              Views:
+              {' '}
+              {photo.views}
+            </>
+          )}
+        </p>
       </div>
       )}
     </main>
--- src/components/TPhoto.js
+++ src/components/TPhoto.js
@@ -11,8 +11,27 @@ const TPhoto = () => {
     <main className="t-photo">
       {photo && (
       <div className="photo">
-        <h1>{photo.id}</h1>
+        {photo.description && (
+          <>
+            <p className="photo-description-h">
+              Description:
+            </p>
+            <p className="photo-description">
+              {photo.description}
+            </p>
+          </>
+        )}
+        <p className="photographer">
+          Photographer:
+          {' '}
+          {photo.user.name}
+        </p>
         <img src={photo.urls.small} alt={photo.alt_description} />
+        <p className="photo-info">
+          Likes:
+          {' '}
+          {photo.likes}
+        </p>
       </div>
       )}
     </main>
